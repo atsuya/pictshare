e219d900b43c0f1f8ed361e28ff3da9337f742dc
Atsuya Takagi <atsuya.takagi@gmail.com>
2010-10-13 15:50:40 -0700

    Added couchdb support.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var PictSharePort = 8080;
 var PictShareFileSizeLimit = 1024 * 1024;
+var PictShareImageCountLimit = 6;
 
 var http = require('http');
 var fs = require('fs');
@@ -9,8 +10,10 @@ var path = require('path');
 
 var staticResource = require('./libraries/static-resource');
 //var mustache = require('./libraries/mustache');
-var io = require('./libraries/socketio')
-var couchdb = require('./libraries/couchdb') 
+var io = require('./libraries/socketio');
+var couchdb = require('./libraries/couchdb'); 
+var couchdbClient = couchdb.createClient(5984, 'localhost');
+var couchdbDb = couchdbClient.db('pictshare');
 
 var handler = staticResource.createHandler(fs.realpathSync('./static'));
 //var mustachedPages = new Array('/index.html');
@@ -40,6 +43,8 @@ var numberOfClients = 0;
 var listener = io.listen(server, {'transports': ['websocket']});
 listener.on('connection', function(client) {
     sendNumberOfClients(1);
+    fillImages(client);
+
     client.on('disconnect', function() {
         sendNumberOfClients(-1);
     });
@@ -72,18 +77,55 @@ function handleUpload(request, response) {
         }
     });
     request.addListener('end', function() {
-        var newData = base64data.toString('base64')/*base64data.toString('base64')*/;
-        listener.broadcast('{"type":"image", "content":"data:image/png;base64,'+newData+'"}');
-        
         response.writeHead(200, {'content-type': 'text/plain'});
         response.end('OK'+'\n\n');
-    });
+
+        var newData = base64data.toString('base64')/*base64data.toString('base64')*/;
+        cleanImages(newData);
+   });
 }
 
 function sendNumberOfClients(change) {
-    sys.debug('sending it.')
     numberOfClients += change;
     listener.broadcast('{"type":"users", "content":"'+numberOfClients+'"}');
+}
+
+function fillImages(client) {
+    couchdbDb.allDocs({}, function(error, data) {
+        console.log(sys.inspect(data));
+        for(var index = 0; index < data.total_rows; index++) {
+            var row = data.rows[index];
+            couchdbDb.getDoc(row.id, function(er, doc) {
+                if(!er) {
+                    client.send('{"type":"image", "content":"data:image/png;base64,'+doc.image+'"}');
+                }
+            });
+        }
+    });
+}
+
+function cleanImages(image) {
+    couchdbDb.allDocs({}, function(error, data) {
+        var imagesToDelete = data.total_rows - PictShareImageCountLimit + 1;
+        for(var index = 0; index < imagesToDelete; index++) {
+            var row = data.rows[index];
+            couchdbDb.removeDoc(row.id, row.value.rev);
+        }
+    });
+
+    saveImage(image);
+}
+
+function saveImage(image) {
+    couchdbClient.request('/_uuids', {count: 1}, function (error, data) {
+        if(!error) {
+            couchdbDb.saveDoc(data.uuids[0], {'image': image}, function(er, ok) {
+                if(!er) {
+                    listener.broadcast('{"type":"image", "content":"data:image/png;base64,'+image+'"}');
+                }
+            });
+        }
+    });
 }
 
 function shutdown() {
